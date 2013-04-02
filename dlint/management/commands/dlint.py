24d8d822518b6e06679ea26be243a2dafc66c84c
Fabio M. Costa <fabiomcosta@gmail.com>
2013-04-01 20:02:13 -0400

    adding the --show-warnings option and adding a resources module that will make importing resources easier

--- dlint/management/commands/dlint.py
+++ dlint/management/commands/dlint.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
+from optparse import make_option
+
 from django.core.management.base import BaseCommand
 from django.template.base import TOKEN_BLOCK, TOKEN_VAR, TemplateSyntaxError
 
-from dlint.finder import TemplateFinder
+from dlint import resources
 
 
 class Command(BaseCommand):
 
+    option_list = BaseCommand.option_list + (
+        make_option("-w", "--show-warnings", action="store_true", dest="show_warnings", default=False, help='Shows warnings.'),
+    )
+
     def handle(self, *args, **options):
 
-        template_finder = TemplateFinder()
-        sources = list(template_finder.items())
+        sources = list(resources.templates.items())
 
         print 'Found {} templates'.format(len(sources))
         print 'analysing unused load templatetag libraries...'
@@ -35,7 +40,7 @@ class Command(BaseCommand):
                     # {{ val|filter }}
 
                     filter_expression = source.parser.compile_filter(token.contents)
-                    filter_functions_in_expression = [f[0] for f in filter_expression.filters]
+                    filter_functions_in_expression += [f[0] for f in filter_expression.filters]
 
                 elif token.token_type == TOKEN_BLOCK:
                     # tags like if, for, etc can use filters anywhere
@@ -57,7 +62,7 @@ class Command(BaseCommand):
                             # while trying to get the filters used on
                             # {% if array1|length == array2|length %}
                             # we will try to compile '==', which will throw
-                            # an TemplateSyntaxError, that will be ignored.
+                            # a TemplateSyntaxError, that will be ignored.
                             pass
 
                 else:
@@ -68,11 +73,12 @@ class Command(BaseCommand):
                     name = getattr(filter_function, '_decorated_function', filter_function).__name__
                     used_filters.add(name)
 
+
             unused_filters = source.loaded_filters.difference(used_filters)
             unused_tags = source.loaded_tags.difference(used_tags)
 
             # TODO make this an option, of course
-            show_warnings = False
+            show_warnings = options['show_warnings']
 
             # TODO separate this into a reporter class
             # that will be a list that will generate outputs
---
+++ dlint/resources/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+from ..finder import TemplateFinder
+
+__all__ = ('templates',)
+
+templates = TemplateFinder()
